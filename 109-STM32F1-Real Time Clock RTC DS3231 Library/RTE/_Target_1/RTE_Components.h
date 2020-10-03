600252159d84900796f5cd78b06a839533792a75
ab11042015 <ahmed.b.yahia@gmail.com>
2020-10-03 22:06:57 +0900

    adding  109-STM32F1-Real Time Clock RTC DS3231 Library

---
+++ 109-STM32F1-Real Time Clock RTC DS3231 Library/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '109-STM32F1-Real Time Clock RTC DS3231 Library' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 109-STM32F1-Real Time Clock RTC DS3231 Library/main.c
@@ -0,0 +1,71 @@
+#include "systick_time.h"
+#include "DS3231_Drive.h"
+#include "oled_drive.h"
+
+/*
+I2C2
+PB10 -> SCL
+PB11 -> SDA
+
+I2C1
+PB6 -> SCL
+PB7 -> SDA
+
+
+*/
+
+
+
+
+char yr = 20;
+char month = 10;
+char day = 3;
+char hrs = 20;
+char min = 4;
+char sec = 40;
+char time_table[7];
+char time_str[12],date_str[12];
+
+
+int main(void)
+{
+	systick_init();
+	
+	
+	oled_init_64(2); // 
+	oled_blank(2);
+	
+	
+	//i2c_init(2,i2c_FM); // If using only the the DS3231 Chip
+
+	//DS3231_update_date_time(2,yr,month,day,hrs,min,sec); // Update the time & Date
+	
+	
+	DelayMs(500);
+	
+
+	
+	while(1)
+	{
+		
+		DS3231_get_time(2,time_table);
+		
+		DS3231_Time_Date(time_table,time_str,Time);// --> Create a time string format hh/mm/ss
+		
+		DS3231_Time_Date(time_table,date_str,Date);// --> Create a date string format yy/mm/dd
+		
+		oled_msg(2,2,35,time_str); // Display the string into the oled
+		
+		oled_msg(2,4,35,date_str); // Display the string into the oled
+		
+		
+		
+		str_empty(date_str);
+		str_empty(time_str);
+		
+		DelayMs(1000);
+	}
+	
+}
+
+
